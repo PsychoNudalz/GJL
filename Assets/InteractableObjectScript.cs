8391013fbde453ad44f9c302d3bbe51bced76cbc
Anson <anson.aw0618@gmail.com>
2021-07-13 00:16:50 +0100

    Anson: added outline shader

--- Assets/InteractableObjectScript.cs
+++ Assets/InteractableObjectScript.cs
@@ -7,19 +7,30 @@ using UnityEngine.Events;
 public class InteractableObjectScript : MonoBehaviour
 {
     [SerializeField] private List<InteractableEvent> interactEvents;
-    
+
+    [Header("Interaction Lock")]
+    [SerializeField] bool lockAfterInteract = true; 
+    [SerializeField] bool interactionLock;
     public bool Interact(Tools t)
     {
+        if (interactionLock)
+        {
+            return false;
+        }
         print($"Interacting with {gameObject.name}");
         UnityEvent e = GetEventByToolEnum(t);
         if (e != null)
         {
             e.Invoke();
+            if (lockAfterInteract)
+            {
+                interactionLock = true;
+            }
             return true;
         }
         else
         {
-            Debug.LogError("Failed to envoke event");
+            Debug.Log("Invalid tool to use");
         }
         return false;
     }
