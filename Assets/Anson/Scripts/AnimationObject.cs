f344f799beed7c196b5bf756e2fb8905c8a9f753
Anson <anson.aw0618@gmail.com>
2021-07-12 23:34:09 +0100

    Anson: added animator object script

--- Assets/Anson/Scripts/AnimationObject.cs
+++ Assets/Anson/Scripts/AnimationObject.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class AnimationObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] Animator animator;
+
+    private void Awake()
     {
-        
+        if (animator== null)
+        {
+            animator = GetComponent<Animator>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play_Trigger(string s)
     {
-        
+        animator.SetTrigger(s);
     }
+    
+    public void Play_Int(string s)
+    {
+        string[] temp = s.Split('/');
+        animator.SetInteger(temp[0], int.Parse(temp[1]));
+    }
+    public void Play_Bool(string s)
+    {
+        string[] temp = s.Split('/');
+        if (int.Parse(temp[1]) == 0)
+        {
+            animator.SetBool(temp[0], false);
+        }
+        else
+        {
+            animator.SetBool(temp[0], true);
+        }
+    }
+    public void Play_Float(string s)
+    {
+        string[] temp = s.Split('/');
+        animator.SetFloat(temp[0], float.Parse(temp[1]));
+    }
+
 }
