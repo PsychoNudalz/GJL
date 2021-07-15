11eeebefb9bd57d3661eee11ea9c1f6f27315408
Daniel Newsom <danielgnewsom@gmail.com>
2021-07-15 22:09:27 +0100

    Danny  - Add compactor!

---
+++ Assets/_Danny/Scripts/EventTriggerScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTriggerScript : MonoBehaviour
+{
+    [SerializeField] private UnityEvent[] eventsToTrigger;
+    [SerializeField] private string[] tagsToCheck;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (string tag in tagsToCheck)
+        {
+            if (other.gameObject.CompareTag(tag))
+            {
+                print($"Collision with - {other.gameObject}");
+                foreach (UnityEvent unityEvent in eventsToTrigger)
+                {
+                    unityEvent.Invoke();
+                }
+                break;
+            }
+        }
+    }
+
+}
