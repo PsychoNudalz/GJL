344eae09253b4ca8a7cd17c2f5196c6446b31177
Psycho_Nudalz <anson.aw0618@gmail.com>
2021-07-16 16:46:32 +0100

    Anson: imported kitchen stuff

--- Assets/_Danny/Scripts/EventTriggerScript.cs
+++ Assets/_Danny/Scripts/EventTriggerScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public class EventTriggerScript : MonoBehaviour
 {
     [SerializeField] private UnityEvent[] eventsToTrigger;
+    [SerializeField] private UnityEvent[] eventsToTriggerOnExit;
     [SerializeField] private string[] tagsToCheck;
 
     private void OnTriggerEnter(Collider other)
@@ -16,6 +17,21 @@ public class EventTriggerScript : MonoBehaviour
             {
                 print($"Collision with - {other.gameObject}");
                 foreach (UnityEvent unityEvent in eventsToTrigger)
+                {
+                    unityEvent.Invoke();
+                }
+                break;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (string tag in tagsToCheck)
+        {
+            if (other.gameObject.CompareTag(tag))
+            {
+                print($"Collision with - {other.gameObject}");
+                foreach (UnityEvent unityEvent in eventsToTriggerOnExit)
                 {
                     unityEvent.Invoke();
                 }
